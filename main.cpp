a6dfc6dd96f0c58863b4ab9d087a4d39490d515a
Albert Tomanek <electron826@gmail.com>
2018-01-30 19:53:43 +0000

    c2

--- main.cpp
+++ main.cpp
@@ -2,27 +2,108 @@
 
 MyApp :: MyApp()
 {
+	this->create_ui();
+}
+
+MyApp :: ~MyApp()
+{
+	/* Widgets */
+	delete this->label;
+	
+	/* Menus */
+	delete this->file_new_item;
+	delete this->file_open_item;
+	delete this->file_saveas_item;
+	delete this->file_quit_item;
+	
+	delete this->file_menu;
+	delete this->file_menu_item;
+	delete this->menubar;
+	
+	/* Main */
+	delete this->grid;
+	delete this->root;
+}
+
+void MyApp :: create_ui()
+{
 	this->root = new Gtk::Window;
 	this->root->set_title("MyApp");
 	this->root->set_border_width(12);
 	
 	this->grid = new Gtk::Table;
 	
+	this->create_menus();
+	
+	/* Window Contents */
 	this->label = new Gtk::Label("Contents");
 	this->label->set_size_request(320, 240);
 	
-	this->grid->attach(*(this->label), 0, 1, 0, 1);
-	
+	this->grid->attach(*(this->label), 0, 1, 1, 2);
+		
 	this->root->add(*this->grid);
 	this->root->show_all();
 }
 
-MyApp :: ~MyApp()
+void MyApp :: create_menus()
 {
-	delete this->label;
+	/* Expects ->grid to have been initialized */
 	
-	delete this->grid;
-	delete this->root;
+	/* File menu */
+	this->menubar   = new Gtk::MenuBar();
+	
+	this->file_menu = new Gtk::Menu();
+	
+	this->file_menu_item = new Gtk::MenuItem("_File", true);
+	this->file_menu_item->set_submenu(*(this->file_menu));
+	
+	this->file_new_item = new Gtk::MenuItem("_New", true);
+	this->file_new_item->signal_activate().connect(sigc::mem_fun(this, &MyApp::new_file));
+	this->file_menu->append(*(this->file_new_item));
+	this->file_open_item = new Gtk::MenuItem("_Open...", true);
+	this->file_open_item->signal_activate().connect(sigc::mem_fun(this, &MyApp::open_dialog));
+	this->file_menu->append(*(this->file_open_item));
+	this->file_saveas_item = new Gtk::MenuItem("Save _as...", true);
+	this->file_saveas_item->signal_activate().connect(sigc::mem_fun(this, &MyApp::saveas_dialog));
+	this->file_menu->append(*(this->file_saveas_item));
+	this->file_quit_item = new Gtk::MenuItem("_Quit", true);
+	this->file_quit_item->signal_activate().connect(sigc::mem_fun(this, &MyApp::quit));
+	this->file_menu->append(*(this->file_quit_item));
+	
+	this->menubar->append(*(this->file_menu_item));
+
+	this->help_menu = new Gtk::Menu();
+	this->help_menu_item = new Gtk::MenuItem("_Help", true);
+	this->help_menu_item->set_right_justified(true);
+	this->help_menu_item->set_submenu(*(this->help_menu));
+
+	this->help_about_item = new Gtk::MenuItem("_About", true);
+	this->help_about_item->signal_activate().connect(sigc::mem_fun(this, &MyApp::about));
+	this->help_menu->append(*(this->help_about_item));
+	
+	this->menubar->append(*(this->help_menu_item));
+	
+	this->grid->attach(*(this->menubar), 0, 1, 0, 1);
+}
+
+void MyApp :: new_file()
+{
+	puts("Create new file");
+}
+
+void MyApp :: open_dialog()
+{
+	this->load("not implemented yet");
+}
+
+void MyApp :: saveas_dialog()
+{
+	this->save("not implemented yet");
+}
+
+void MyApp :: about()
+{
+	puts("About");
 }
 
 void MyApp :: load(char *path)
@@ -38,6 +119,11 @@ void MyApp :: save(char *path)
 void MyApp :: run()
 {
 	Gtk::Main::run(*this->root);	
+}
+
+void MyApp :: quit()
+{
+	Gtk::Main::quit();
 }
 
 int main(int argc, char *argv[])
--- main.h
+++ main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 
 #include <gtkmm.h>
+#include <gtkmm/window.h>
 
 class MyApp
 {
@@ -9,15 +10,41 @@ class MyApp
 	Gtk::Table  *grid;
 	
 	Gtk::Label *label;
-		
+	
+	/* File Menu */
+	Gtk::MenuBar  *menubar;
+	
+	Gtk::Menu     *file_menu;
+	Gtk::MenuItem *file_menu_item;
+	
+	Gtk::MenuItem *file_new_item;
+	Gtk::MenuItem *file_open_item;
+	Gtk::MenuItem *file_saveas_item;
+	Gtk::MenuItem *file_quit_item;
+
+	Gtk::Menu     *help_menu;
+	Gtk::MenuItem *help_menu_item;
+	
+	Gtk::MenuItem *help_about_item;
+	
+	/* Methods */
+	void create_ui();
+	void create_menus();
+			
 public:
 	MyApp();
 	~MyApp();
+	
+	void new_file();
+	void open_dialog();
+	void saveas_dialog();
+	void about();
 	
 	void load(char *path);
 	void save(char *path);
 	
 	void run();
+	void quit();
 };
 
 #endif
